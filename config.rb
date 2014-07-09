0fa35e6d14e297b84724a0947f08e432b2792f29
RaeMilne <rmilne@sva.edu>
2014-07-09 19:16:34 -0400

    Woohoo! Fixed the folder problem

--- config.rb
+++ config.rb
@@ -1,9 +1,11 @@
 require 'susy'
 preferred_syntax = :sass
 http_path = '/'
-css_dir = '_/css'
+css_dir = 'css'
 sass_dir = '_/components/sass'
 images_dir = 'images'
-javascripts_dir = '_/js'
+javascripts_dir = 'js'
 relative_assets = true
 output_style = :nested
+
+
